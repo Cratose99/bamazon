e81ecd804cd70f6b24f95abfdc8403a226705063
Cratose99 <gabriel.eyer007@gmail.com>
2019-11-23 02:53:00 -0800

    added db

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+USE bamazon_DB;
+
+
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Watermelon","food",5.33, 50);
+
+INSERT INTO products(product_name, department_name,price, stock_quantity)
+VALUES ("Swiss Cheese","food",3.99, 100);
+
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("4k OLED TV","electronics",1900, 100);
+
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Dinosoar","reptiles", 100000, 1);
+
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("1993 Toyots MR2","car", 100000, 1);
+
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Xbox Game pass","redeemable",1, 50);
+
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Sushi Rolls","food", 12.99, 10);
+
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Gaming Chair","furniture",300, 45);
+
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Mountain Dew Case","food",5.99, 10000);
